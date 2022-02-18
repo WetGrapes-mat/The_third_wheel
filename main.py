bc65e009b351be695d66357568587201094d7844
Polina <polinavinograd40@gmail.com>
2022-02-18 22:52:37 +0300

    Add input

--- main.py
+++ main.py
@@ -12,5 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+if input() == 'some':
+    print('BODY ONCE TOLD ME')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
