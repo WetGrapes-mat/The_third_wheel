fbdd94a0d0d0d673d875fe226ea1232b9628095b
Your NameWetGrapes-mat <matvey.diubuilo@icloud.com>
2022-02-21 17:37:59 +0300

    review

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-class Poliypisca:
-    pass
+from server_wot import Server
+
+s = Server()
+
+player = s.get_player_list()[0]
+
+player.lets_battle(s)
 
 
-def matvey_vonyuchka():
-    print('')
+# player.change_nickname()
+# player.buy_tank(s)
+
+
+s.set_players_in_file(s.get_player_list())
+
+
